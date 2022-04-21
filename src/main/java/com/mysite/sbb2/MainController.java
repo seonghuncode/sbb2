7db2e46d633cdfea1186d52487813030333de833
seonghuncode <gnos456@naver.com>
2022-04-22 03:44:34 +0900

    세팅

---
+++ src/main/java/com/mysite/sbb2/MainController.java
@@ -0,0 +1,14 @@
+package com.mysite.sbb2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @RequestMapping("/sbb")
+    @ResponseBody
+    public String index(){
+        return "안녕하세요 sbb에 오신것을 환영 합니다.";
+    }
+}
---
+++ src/main/java/com/mysite/sbb2/Sbb2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb2Application.class, args);
+	}
+
+}
