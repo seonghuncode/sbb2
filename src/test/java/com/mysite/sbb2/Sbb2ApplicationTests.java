c72a8ed3a616aaab4265655ed209e7ba83a392ae
seonghuncode <gnos456@naver.com>
2022-04-23 15:53:42 +0900

    2-05 리포지터리, 데이터 조회하기, findById

--- src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,7 +17,19 @@ class Sbb2ApplicationTests {
 	private QuestionRepository questionRepository;
 
 	@Test
-	void testJpa() {
+
+	void testFindById() {
+		Optional<Question> oq = questionRepository.findById(1);
+
+		if (oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	@Test
+	void testFindAll() {
+
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 
