1027e88559e8c19948db0f6e83fd2ea5192b40f7
seonghuncode <gnos456@naver.com>
2022-04-23 23:34:00 +0900

    2-08 ROOT URL

--- src/main/java/com/mysite/sbb2/MainController.java
+++ src/main/java/com/mysite/sbb2/MainController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @RequestMapping("/sbb")
     @ResponseBody
-
-
-
     public String index(){
         return "안녕하세요 sbb에 오신것을 환영 합니다.";
     }
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
 }
