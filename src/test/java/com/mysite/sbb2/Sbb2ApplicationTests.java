9d1dab0b554840075cbef97734f22f5a994575a0
seonghuncode <gnos456@naver.com>
2022-04-23 17:29:02 +0900

    2-05 리포지터리, 연관된 답변, 연관된 질문, Transactional

--- src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
+++ src/test/java/com/mysite/sbb2/Sbb2ApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +19,29 @@ class Sbb2ApplicationTests {
 
 	@Autowired
 	private AnswerRepository answerRepository;
+
+	@Test
+	@Transactional
+	void testFindRelatedAnswers() {
+		// SELECT * FROM question WHERE id = 2;
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		// SELECT * FROM answer WHERE question_id = 2;
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
+
+	@Test
+	void testQuestionFind() {
+		Optional<Answer> oa = answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(2, a.getQuestion().getId());
+	}
 
 	@Test
 	void testCreateAnswer() {
