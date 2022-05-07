b1d707ed7ac5443aaeb488cf9cdbc6a1cc39a282
seonghuncode <gnos456@naver.com>
2022-05-07 20:57:05 +0900

    3-10 수정과 삭제, 답변 삭제, 수정일시 표시

--- src/main/java/com/mysite/sbb2/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb2/answer/AnswerController.java
@@ -80,6 +80,19 @@ public class AnswerController {
         return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(@PathVariable("id") int id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        answerService.delete(answer);
+
+        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+    }
+
 
 
 
--- src/main/java/com/mysite/sbb2/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb2/answer/AnswerService.java
@@ -43,4 +43,8 @@ public class AnswerService {
         answerRepository.save(answer);
     }
 
+    public void delete(Answer answer) {
+        answerRepository.delete(answer);
+    }
+
 }
