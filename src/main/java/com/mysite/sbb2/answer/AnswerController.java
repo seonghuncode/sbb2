165760969dd20298ff6d15796dffda77c97f6741
seonghuncode <gnos456@naver.com>
2022-05-07 20:51:58 +0900

    3-10 수정과 삭제, 답변 수정

--- src/main/java/com/mysite/sbb2/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb2/answer/AnswerController.java
@@ -5,13 +5,16 @@ import com.mysite.sbb2.question.QuestionService;
 import com.mysite.sbb2.user.SiteUser;
 import com.mysite.sbb2.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -44,6 +47,37 @@ public class AnswerController {
 
         answerService.create(question, answerForm.getContent(), author);
         return "redirect:/question/detail/%d".formatted(id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") int id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+        Answer answer = answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerService.modify(answer, answerForm.getContent());
+
+        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
     }
 
 
--- src/main/java/com/mysite/sbb2/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb2/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb2.answer;
 
+import com.mysite.sbb2.question.DataNotFoundException;
 import com.mysite.sbb2.question.Question;
 import com.mysite.sbb2.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,23 @@ public class AnswerService {
         answer.setAuthor(author);
         answer.setAuthor(author);
 
+        answerRepository.save(answer);
+    }
+
+
+    public Answer getAnswer(int id) {
+        Optional<Answer> answer = answerRepository.findById(id);
+
+        if ( answer.isPresent() ) {
+            return answer.get();
+        }
+
+        throw new DataNotFoundException("answer not found");
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
         answerRepository.save(answer);
     }
 
